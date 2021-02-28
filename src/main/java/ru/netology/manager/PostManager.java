13fbfbee3e34c8cd24d0ecf43d4891d5f352bc72
Olga Tarasova <bos.olga18@gmail.com>
2021-02-28 22:28:12 +0300

    Added manager

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,14 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] wallSearch(int ownerId, String domain, String query, int ownersOnly, int count, int offset) {
+        return null;
+    }
+    public void wallDelete(int ownerId, int postId) {
+        return;
+    }
+}
